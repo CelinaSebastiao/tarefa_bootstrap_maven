7c61f40df4b7228a3f98befba8f76b12874c11f9
Celina Sebastiao <celinasebastiao73@gmail.com>
2020-09-11 12:53:19 +0100

    Actualizado com projecto Maven

---
+++ src/main/java/gestaorh/app/ensino_bootstrap/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package gestaorh.app.ensino_bootstrap;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
